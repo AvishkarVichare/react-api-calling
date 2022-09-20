ac555a75d1fd0d2707187def6e8118a2c571a3c8
Avishkar Vichare <avie9500@gmail.com>
2022-09-20 23:27:36 +0530

    initial and final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
 
-function App() {
+const App = () => {
+
+    const [list, setList] = useState([]);
+
+useEffect(() => {
+
+    const fetchApi = async()=>{
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await res.json();
+        setList(data);
+    }
+    fetchApi()
+
+}, [])
+
+// console.log(list)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='flex flex-col gap-3'>
+        <h1 className='text-center font-extrabold text-[20px] my-6'>Listed Items using api json placeholder</h1>
+    {list.map((e,index)=>{
+        return(
+        <div  className='bg-gray-300 w-full p-3 mx-4' key={index}>
+            <div>
+            <span className='font-extrabold'>{e.id}.</span> <span className='font-bold'>{e.title}</span>
+            </div>
+            <div >
+                {e.body}
+            </div>
+        </div>
+        )
+    })}
+   </div>
+  )
 }
 
-export default App;
+export default App
